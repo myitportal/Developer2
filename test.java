d19aaa4cff038c64aeb3f43d34ac0e85260ee4d0
myitportal <68616171+myitportal@users.noreply.github.com>
2022-12-05 09:25:02 +0000

    Update test.java

--- test.java
+++ test.java
@@ -2,4 +2,4 @@ public class Simple{
 	public static void main(String args[]){
 		System.out.println("hello javatpoint");
 	}}
-A little java script examples for training hopefully it helps
+A little java script examples for training hopefully it helps. Goodluck.
